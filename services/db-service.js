09f8527e7c1f5a5ffb817e2ffe29dc7f7ac2b08a
TimmyTheGamer <timothybowen9@gmail.com>
2019-05-13 15:53:59 +1000

    stuffzzz

---
+++ services/db-service.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var messageSchema = new Schema({
+  message: String,
+  userID: String,
+  date: {type: Date, default: Date.now},
+  conversationID: Number
+});
+
+var userSchema = new Schema({
+  userID: String,
+  password: String,
+  firstName: String,
+  lastName: String,
+  dob: Date,
+  meta: {
+    interests: Array,
+    Strengths: Array
+  }
+});
